f58ad6b799d751c23a30906ba892b0c4196a64c4
翠花 <im@nan.ge>
2020-07-08 21:27:55 +0800

    Update and rename config.example.php to config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('HOST_KEY', 'e9e4498f0584b7098692512db0c62b48');
+define('HOST_MAIL', 'admin@nange.cn');
+$page_title = "楠格"; // Optional. Should not use HTML special character.
+$tlo_path = '/'; // Optional. The installation path for this panel, ending with '/'. Required for HTTP/2 Push.
+// $is_debug = false; // Enable debug mode
