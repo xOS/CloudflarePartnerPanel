87e168b38337c2d9804e6c6029747a546446f4ce
翠花 <admin@nange.cn>
2022-02-12 04:04:54 +0800

    Update.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('HOST_KEY', 'e9e4498f0584b7098692512db0c62b48');
-define('HOST_MAIL', 'admin@nange.cn');
+define('HOST_KEY', '');
+define('HOST_MAIL', '');
 $page_title = "楠格"; // Optional. Should not use HTML special character.
 $tlo_path = '/'; // Optional. The installation path for this panel, ending with '/'. Required for HTTP/2 Push.
 // $is_debug = false; // Enable debug mode
